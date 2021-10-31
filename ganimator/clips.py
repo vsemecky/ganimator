9be9c98bf4e51af92e89e9ad8f369bbe48156f86
Vojtech Semecky <vojtech@semecky.cz>
2021-10-31 17:44:09 +0100

    clips.py + functions.py

---
+++ ganimator/clips.py
@@ -0,0 +1,319 @@
+import sys
+
+# Allow import from git submodules
+sys.path.append("./submodules/stylegan2-ada/")
+
+import glob
+import re
+import scipy
+from PIL import Image, ImageFont, ImageDraw
+from moviepy.editor import *
+from .functions import *
+
+
+class StaticImageClip(ImageClip):
+    """ Single static image generated from GAN. This clip will simply display the same image at all times."""
+
+    def __init__(
+            self,
+            pkl: str,
+            duration: int = 30,
+            seed: int = 42,
+            trunc: float = 1,
+            randomize_noise: bool = False,
+            title=None,
+            title_font_size=None,
+    ):
+        pil_image = generate_image(pkl=pkl, seed=seed, trunc=trunc, randomize_noise=randomize_noise)
+
+        if title is not None:
+            # Append title text
+            height = 768  # temporary hack
+            title_font_size = title_font_size or height // 32
+            title_font = get_image_font(family='sans-serif', weight='normal', size=title_font_size)
+            draw = ImageDraw.Draw(pil_image, 'RGBA')  # RGBA because of semitransparent rectangle arount the title
+            draw_text(draw=draw, image=pil_image, font=title_font, text=title, gravity="South", fill=(0, 0, 0, 200), margin=height // 64, padding=title_font_size // 5)
+
+        super().__init__(np.array(pil_image), duration=duration)
+
+
+class LatentWalkClip(VideoClip):
+    """ Generates Random latent walk clip """
+
+    def __init__(
+            self,
+            pkl: str,
+            duration: int = 30,
+            seed: int = 42,
+            trunc: float = 1.0,
+            randomize_noise: bool = False,
+            smoothing_sec: float = 1.0,
+            fps: int = 30,
+            title=None,
+            title_font_size=None,
+    ):
+        # Nepouzivane parametry z puvodni funkce
+        grid_size = [1, 1]
+        tflib.init_tf()
+        Gs = load_network_Gs(pkl)
+        height, width = Gs.output_shape[2:]
+        fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
+
+        num_frames = int(np.rint(duration * fps))
+        random_state = np.random.RandomState(seed)
+
+        # Generating latent vectors
+        shape = [num_frames, np.prod(grid_size)] + Gs.input_shape[1:]  # [frame, image, channel, component]
+        all_latents = random_state.randn(*shape).astype(np.float32)
+        all_latents = scipy.ndimage.gaussian_filter(all_latents, [smoothing_sec * fps] + [0] * len(Gs.input_shape), mode='wrap')
+        all_latents /= np.sqrt(np.mean(np.square(all_latents)))
+        if title is not None:
+            title_font_size = title_font_size or height // 32
+            title_font = get_image_font(family='sans-serif', weight='normal', size=title_font_size)
+
+        def make_frame(t):
+            """ Frame generation func for MoviePy """
+            frame_idx = int(np.clip(np.round(t * fps), 0, num_frames - 1))
+            latents = all_latents[frame_idx]
+            images = Gs.run(latents, None, truncation_psi=trunc, randomize_noise=randomize_noise, output_transform=fmt)
+            image = images[0]
+            if title is None:
+                return image
+
+            # Append title text
+            pil_image = Image.fromarray(image)
+            draw = ImageDraw.Draw(pil_image, 'RGBA')  # RGBA because of semitransparent rectangle arount the title
+            draw_text(draw=draw, image=pil_image, font=title_font, text=title, gravity="South", fill=(0, 0, 0, 200), margin=height // 64, padding=title_font_size // 5)
+
+            return np.array(pil_image)
+
+        # Create VideoClip
+        super().__init__(make_frame=make_frame, duration=duration)
+
+
+class InterpolationClip(VideoClip):
+    """ Generates interpolation video between seeds """
+
+    def __init__(
+            self,
+            pkl: str,
+            duration: int = None,
+            step_duration: int = 3,
+            seeds: list = [1, 2, 3],
+            trunc: float = 1.0,
+            randomize_noise: bool = False,
+            fps: int = 30
+    ):
+        if duration is None:
+            duration = step_duration * (len(seeds) - 1)
+
+        tflib.init_tf()
+        Gs = load_network_Gs(pkl)
+
+        noise_vars = [var for name, var in Gs.components.synthesis.vars.items() if name.startswith('noise')]
+
+        zs = generate_zs_from_seeds(seeds, Gs)
+
+        num_frames = int(np.rint(duration * fps))
+        number_of_steps = int(num_frames / (len(zs) - 1)) +1  # todo Prejmenovat na num_steps nebo steps_count/frames_count
+        points = line_interpolate(zs, number_of_steps)
+
+        Gs_kwargs = dnnlib.EasyDict()
+        Gs_kwargs.output_transform = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
+        Gs_kwargs.randomize_noise = randomize_noise
+        Gs_kwargs.truncation_psi = trunc
+
+        # Frame generation func for moviepy
+        def make_frame(t):
+            frame_idx = int(np.clip(np.round(t * fps), 0, num_frames - 1))
+
+            # TIP: Oddebugovat run_generator,  co mu sem leze, zejmena len(zx)
+            z_idx = frame_idx
+            z = points[z_idx]
+
+            # Puvodni loop
+            if isinstance(z, list):
+                z = np.array(z).reshape(1, 512)
+            elif isinstance(z, np.ndarray):
+                z.reshape(1, 512)
+
+            noise_rnd = np.random.RandomState(1)  # fix noise
+            tflib.set_vars({var: noise_rnd.randn(*var.shape.as_list()) for var in noise_vars})  # [height, width]
+            images = Gs.run(z, None, **Gs_kwargs)  # [minibatch, height, width, channel]
+            return images[0]
+
+        # Create VideoClip
+        super().__init__(make_frame=make_frame, duration=duration)
+
+
+class ArrayClip(CompositeVideoClip):
+    """
+    Object-oriented implementation of MoviePy function array_clip()
+
+    rows_widths
+      widths of the different rows in pixels. If None, is set automatically.
+
+    cols_widths
+      widths of the different colums in pixels. If None, is set automatically.
+
+    bg_color
+       Fill color for the masked and unfilled regions. Set to None for these
+       regions to be transparent (will be slower).
+
+    """
+    def __init__(self, array, rows_widths=None, cols_widths=None, bg_color=None):
+        array = np.array(array)
+        sizes_array = np.array([[c.size for c in line] for line in array])
+
+        # find row width and col_widths automatically if not provided
+        if rows_widths is None:
+            rows_widths = sizes_array[:, :, 1].max(axis=1)
+        if cols_widths is None:
+            cols_widths = sizes_array[:, :, 0].max(axis=0)
+
+        xx = np.cumsum([0] + list(cols_widths))
+        yy = np.cumsum([0] + list(rows_widths))
+
+        for j, (x, cw) in enumerate(zip(xx[:-1], cols_widths)):
+            for i, (y, rw) in enumerate(zip(yy[:-1], rows_widths)):
+                clip = array[i, j]
+                w, h = clip.size
+                if (w < cw) or (h < rw):
+                    clip = (CompositeVideoClip([clip.set_position('center')],
+                                               size=(cw, rw),
+                                               bg_color=bg_color).
+                            set_duration(clip.duration))
+
+                array[i, j] = clip.set_position((x, y))
+
+        super().__init__(array.flatten(), size=(xx[-1], yy[-1]), bg_color=bg_color)
+
+
+class TruncComparisonClip(ArrayClip):
+    """  """
+
+    def __init__(
+            self,
+            pkl=None,
+            grid=(3, 2),  # (cols, rows)
+            trunc_range=(0.0, 1.0),  # (trunc_min, trunc_max)
+            fps=30,  # frames per second
+            duration=10,  # Duration in seconds
+            smoothing_sec=1.0,
+            randomize_noise=False,
+            caption=True,  # Display truncation psi at the bottom if each image
+            seed=420  # Starting seed of the first image
+    ):
+        cols, rows = grid
+        trunc_min, trunc_max = trunc_range
+        step = (trunc_max - trunc_min) / (cols * rows - 1)
+
+        # empty array rows x cols
+        clips = [[0 for col in range(cols)] for row in range(rows)]
+
+        i = 0
+        for row in range(rows):
+            for col in range(cols):
+                trunc = trunc_min + i * step
+                i += 1
+                height = 768  # temporary hack (until drivers will be done)
+                if trunc == 0:  # Save some time: Truncation psi=0 generates allways the same average image.
+                    clips[row][col] = StaticImageClip(
+                        pkl=pkl,
+                        seed=seed,
+                        trunc=trunc,
+                        duration=duration,
+                        randomize_noise=randomize_noise,
+                        title="psi 0.0",
+                        title_font_size=rows * height // 32,
+                    )
+                else:
+                    clips[row][col] = LatentWalkClip(
+                        pkl=pkl,
+                        seed=seed,
+                        trunc=trunc,
+                        duration=duration,
+                        randomize_noise=randomize_noise,
+                        smoothing_sec=smoothing_sec,
+                        fps=fps,
+                        title="psi " + str(round(trunc, 2)),
+                        title_font_size=rows * height // 32,
+                    )
+
+        # Arrange clips into ArrayClip (parent class)
+        super().__init__(clips)
+
+
+class ProgressClip(VideoClip):
+
+    def __init__(self, fakes_dir, fps=30, fps_step=None, height=1080, heading="Training Progress", subheading="StyleGAN"):
+
+        VideoClip.__init__(self)
+
+        sequence = sorted(glob.glob(f"{fakes_dir}/fakes??????.jpg"))  # todo: support also png
+        self.fps = fps
+        width = height * 16 // 9  # Calc `width` to keep ratio 16:9
+        self.durations = []
+        self.images_starts = []
+        pointer = 0
+        for i, val in enumerate(sequence):
+            if fps_step:  # increasing fps
+                fps_moving = min(fps_step * i + 1, fps)
+                # print(i, "fps_moving", fps_moving)
+                duration = 1.0 / fps_moving
+            else:  # fix fps
+                duration = 1.0 / fps
+            self.durations.append(duration)
+            self.images_starts.append(pointer)
+            pointer += duration
+
+        self.duration = sum(self.durations)
+        self.end = self.duration
+        self.sequence = sequence
+        self.lastindex = None
+        self.lastimage = None
+
+        # Calc text block height
+        pil_fake = Image.open(self.sequence[0])  # get test image
+        pil_fake.thumbnail(size=(width, height))
+        text_block_height = height - pil_fake.height
+
+        # Prepare fonts
+        heading_font = ImageFont.truetype(font_by_name('sans-serif', 'bold'), size=text_block_height // 4)
+        subhead_font = ImageFont.truetype(font_by_name('sans-serif', 'normal'), size=text_block_height // 6)
+        counter_font = ImageFont.truetype(font_by_name('monospace', 'normal'), size=text_block_height // 6)
+
+        # Background image with text headings
+        pil_background = Image.new(mode="RGB", size=(width, height), color="black")  # New empty image
+        draw = ImageDraw.Draw(pil_background)
+        # Heading text
+        heading_w, heading_h = draw.textsize(heading, font=heading_font)
+        draw.text(xy=((width - heading_w) // 2, text_block_height // 20), text=heading, fill=(255, 255, 255), font=heading_font, align="center")
+        # Sub-Heading text
+        subh_w, subh_h = draw.textsize(subheading, font=subhead_font)
+        draw.text(xy=((width - subh_w) // 2, heading_h + text_block_height // 10), text=subheading, fill=(255, 255, 255), font=subhead_font, align="center")
+
+        def make_frame(t):
+            index = max(
+                [i for i in range(len(self.sequence)) if self.images_starts[i] <= t]
+            )
+
+            if index != self.lastindex:
+                pil_image = pil_background.copy()
+                pil_fake = Image.open(self.sequence[index])
+                pil_fake.thumbnail(size=(width, height))
+                pil_image.paste(pil_fake, (0, text_block_height))
+
+                # Counter text [kimg]
+                draw = ImageDraw.Draw(pil_image)
+                kimg = re.search(r'\d+', self.sequence[index]).group()
+                text = f"{kimg} kimg"
+                text_w, text_h = draw.textsize(text, font=counter_font)
+                draw.text(xy=((width - text_w) // 2, text_block_height - text_h - text_block_height // 10), text=text, fill=(255, 255, 255), font=counter_font, align="center")
+
+                self.lastimage = np.array(pil_image)
+                self.lastindex = index
+            return self.lastimage  # Prevest na array jen jednou a ulozit do lastimage
+
+        self.make_frame = make_frame
+        self.size = make_frame(0).shape[:2][::-1]
--- ganimator/functions.py
+++ ganimator/functions.py
@@ -1,2 +1,194 @@
-def myfunction():
-    return "bla"
+# Allow import from git submodules
+import sys
+# sys.path.append("./submodules/stylegan2-ada/")
+
+import time
+from datetime import datetime
+import numpy as np
+from PIL import Image, ImageFont, ImageDraw
+import progressbar
+from matplotlib import font_manager
+from moviepy.editor import *
+
+
+#
+# Get font path by font-family name
+#
+def font_by_name(family='sans-serif', weight='normal') -> str:
+    return font_manager.findfont(
+        font_manager.FontProperties(family=family, weight=weight)
+    )
+
+
+#
+# Get PIL ImageFont by options
+#
+def get_image_font(family='sans-serif', weight='normal', size=12) -> ImageFont:
+    font_path = font_manager.findfont(
+        font_manager.FontProperties(family='sans-serif', weight='normal')
+    )
+    return ImageFont.truetype(font_path, size=size)
+
+
+#
+# Draw textbox on PIL Image
+#
+def draw_text(draw: ImageDraw, image: Image, font, text="Text example", gravity="South", fill=(0, 0, 0), padding=5, margin=10):
+    text_width, text_height = draw.textsize(text, font=font)
+    gravity = gravity.lower()
+
+    if gravity == 'south':
+        x = (image.width - text_width) // 2
+        y = image.height - text_height - margin - padding
+    elif gravity == 'north':
+        x = (image.width - text_width) // 2
+        y = margin + padding
+    elif gravity == 'center':
+        x = (image.width - text_width) // 2
+        y = (image.height - text_height) // 2
+    elif gravity == 'southwest':
+        x = margin + padding
+        y = image.height - text_height - margin - padding
+    elif gravity == 'southeast':
+        x = image.width - margin - padding - text_width
+        y = image.height - text_height - margin - padding
+    elif gravity == 'northwest':
+        x = y = margin + padding
+    elif gravity == 'northeast':
+        x = image.width - margin - padding - text_width
+        y = margin + padding
+    else:
+        x = y = 0
+
+    draw.rectangle((x - padding, y - padding, x + text_width + padding, y + text_height + padding), fill=fill)
+    draw.text((x, y), text=text, font=font)
+
+
+#
+# Generates unified video filename based on opional parameters
+#
+def generate_video_filename(dir=None, dataset=None, timestamp=False, name="video", seed=None, duration=None, trunc=None, pkl=None):
+    if dir:
+        file_name = f"/{dir}/"
+    else:
+        file_name = ""
+
+    if dataset:
+        file_name += dataset.replace("/", "-")
+    if pkl:
+        file_name += time.strftime(' - %Y-%m-%d', time.localtime(os.path.getmtime(pkl)))
+    if timestamp:
+        file_name += datetime.now().strftime(" - %Y-%m-%d %H:%M")
+    if name:
+        file_name += " - " + name.replace("/", "-")
+    if seed:
+        file_name += " - seed={}".format(seed)
+    if duration:
+        file_name += " - {}sec".format(duration)
+    if trunc:
+        file_name += " - trunc={:03d}".format(int(100 * trunc))
+    file_name += ".mp4"  # Append extension
+
+    return file_name
+
+
+# from https://colab.research.google.com/drive/1ShgW6wohEFQtqs_znMna3dzrcVoABKIH
+def generate_zs_from_seeds(seeds, Gs):
+    zs = []
+    for seed_idx, seed in enumerate(seeds):
+        rnd = np.random.RandomState(seed)
+        z = rnd.randn(1, *Gs.input_shape[1:])  # [minibatch, component]
+        zs.append(z)
+    return zs
+
+
+def line_interpolate(zs, steps):
+    out = []
+    for i in range(len(zs) - 1):
+        for index in range(steps):
+            fraction = index / float(steps)
+            out.append(zs[i + 1] * fraction + zs[i] * (1 - fraction))
+    return out
+
+
+def generate_image(driver: IDriver, seed: int = 42, trunc: float = 1, randomize_noise: bool = False) -> Image:
+    pass
+
+
+def generate_image_tf(pkl: str, seed: int = 42, trunc: float = 1, randomize_noise: bool = False) -> Image:
+    """ Generate single image and returns PIL Image """
+
+    tflib.init_tf()
+    Gs = load_network_Gs(pkl)  # Loading neurals
+    noise_vars = [var for name, var in Gs.components.synthesis.vars.items() if name.startswith('noise')]
+
+    Gs_kwargs = dnnlib.EasyDict()
+    Gs_kwargs.output_transform = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
+    Gs_kwargs.randomize_noise = randomize_noise
+    Gs_kwargs.truncation_psi = trunc
+
+    rnd = np.random.RandomState(seed)
+    z = rnd.randn(1, *Gs.input_shape[1:])  # [minibatch, component]
+    tflib.set_vars({var: rnd.randn(*var.shape.as_list()) for var in noise_vars})  # [height, width]
+    images = Gs.run(z, None, **Gs_kwargs)  # [minibatch, height, width, channel]
+    image_pil = Image.fromarray(images[0], 'RGB')
+    return image_pil
+
+
+def generate_images(pkl, seeds=None, trunc=None, output_dir=None, ext="jpg"):
+    os.makedirs(output_dir, exist_ok=True)
+    if seeds is None:
+        seeds = [1, 2, 3, 4, 5]
+    for seed in progressbar.progressbar(seeds, redirect_stdout=True):
+        print(f'Generating image (seed={seed}, trunc={trunc})')
+        img = generate_image(pkl=pkl, seed=seed, trunc=trunc)
+        img.save(f"{output_dir}/{seed}.{ext}")
+
+
+def style_mixing_grid(pkl, row_seeds, col_seeds, truncation_psi, col_styles, outdir, minibatch_size=4):
+    tflib.init_tf()
+    Gs = load_network_Gs(pkl)  # Loading neurals
+
+    w_avg = Gs.get_var('dlatent_avg') # [component]
+    Gs_syn_kwargs = {
+        'output_transform': dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True),
+        'randomize_noise': False,
+        'minibatch_size': minibatch_size
+    }
+
+    print('Generating W vectors...')
+    all_seeds = list(set(row_seeds + col_seeds))
+    all_z = np.stack([np.random.RandomState(seed).randn(*Gs.input_shape[1:]) for seed in all_seeds]) # [minibatch, component]
+    all_w = Gs.components.mapping.run(all_z, None) # [minibatch, layer, component]
+    all_w = w_avg + (all_w - w_avg) * truncation_psi # [minibatch, layer, component]
+    w_dict = {seed: w for seed, w in zip(all_seeds, list(all_w))} # [layer, component]
+
+    print('Generating images...')
+    all_images = Gs.components.synthesis.run(all_w, **Gs_syn_kwargs) # [minibatch, height, width, channel]
+    image_dict = {(seed, seed): image for seed, image in zip(all_seeds, list(all_images))}
+
+    print('Generating style-mixed images...')
+    for row_seed in row_seeds:
+        for col_seed in col_seeds:
+            w = w_dict[row_seed].copy()
+            w[col_styles] = w_dict[col_seed][col_styles]
+            image = Gs.components.synthesis.run(w[np.newaxis], **Gs_syn_kwargs)[0]
+            image_dict[(row_seed, col_seed)] = image
+
+    print('Saving image grid...')
+    _N, _C, H, W = Gs.output_shape
+    canvas = Image.new('RGB', (W * (len(col_seeds) + 1), H * (len(row_seeds) + 1)), 'black')
+    for row_idx, row_seed in enumerate([None] + row_seeds):
+        for col_idx, col_seed in enumerate([None] + col_seeds):
+            if row_seed is None and col_seed is None:
+                continue
+            key = (row_seed, col_seed)
+            if row_seed is None:
+                key = (col_seed, col_seed)
+            if col_seed is None:
+                key = (row_seed, row_seed)
+            canvas.paste(Image.fromarray(image_dict[key], 'RGB'), (W * col_idx, H * row_idx))
+
+    string_styles = [str(i) for i in col_styles]
+    styles_str = "".join(string_styles)
+    canvas.save(f'{outdir}/grid-{styles_str}.png')
