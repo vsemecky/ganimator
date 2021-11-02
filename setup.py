9180e61a57e3df402b7459698638692b7757365b
Vojtech Semecky <vojtech@semecky.cz>
2021-11-02 23:09:38 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/vsemecky/ganimator',
     license='MIT',
-    packages=['ganimator'],
+    packages=setuptools.find_packages(),
     install_requires=requirements,
 )
