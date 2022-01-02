a08330ecb7ec2d800d5f9dc069cfab0b3d6d44b1
Vojtech Semecky <vojtech@semecky.cz>
2022-01-02 02:08:59 +0100

    In progress...

--- ganimator/__init__.py
+++ ganimator/__init__.py
@@ -1,3 +1,9 @@
+import os
+import sys
+ganimator_dir = os.path.dirname(os.path.abspath(__file__))
+print("ganimator_dir", ganimator_dir)
+sys.path.append(ganimator_dir)
+
 from . import functions
 from .IDriver import IDriver
 from .StyleGanTfDriver import StyleGanTfDriver
@@ -5,9 +11,3 @@ from .StyleGanPyDriver import StyleGanPyDriver
 from .StyleGanPyExperimentalDriver import StyleGanPyExperimentalDriver
 from .clips import *
 
-import os
-import sys
-
-ganimator_dir = os.path.dirname(os.path.abspath(__file__))
-print("ganimator_dir", ganimator_dir)
-sys.path.append(ganimator_dir)
