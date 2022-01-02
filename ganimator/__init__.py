bc00b6cb51540f6c802451b2f43dfcc77d1de531
Vojtech Semecky <vojtech@semecky.cz>
2022-01-02 01:54:27 +0100

    Append Ganimator dir to syspath

--- ganimator/__init__.py
+++ ganimator/__init__.py
@@ -1,6 +1,13 @@
 from . import functions
 from .IDriver import IDriver
 from .StyleGanTfDriver import StyleGanTfDriver
-# from .StyleGanPyDriver import StyleGanPyDriver
-# from .StyleGanDriverFun import StyleGanDriverFun
+from .StyleGanPyDriver import StyleGanPyDriver
+from .StyleGanPyExperimentalDriver import StyleGanPyExperimentalDriver
 from .clips import *
+
+import os
+import sys
+
+ganimator_dir = os.path.dirname(os.path.abspath(__file__))
+print("ganimator_dir", ganimator_dir)
+sys.path.append(ganimator_dir)
